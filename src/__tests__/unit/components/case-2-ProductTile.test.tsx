ec8b400b1f9ac3f71ba89fa0114913f1ea8581d4
solomonakp <solomonakp@gmail.com>
2021-01-27 14:02:22 +0100

    query and getBy

--- src/__tests__/unit/components/case-2-ProductTile.test.tsx
+++ src/__tests__/unit/components/case-2-ProductTile.test.tsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import { axe } from 'jest-axe'
-import { render } from '@testing-library/react'
+import { getAllByText, queryByAltText, render } from '@testing-library/react'
 import ProductTile from '../../../components/ProductTile'
 
+const defaultProduct = {
+  id: 12,
+  name: 'Example product name',
+  image: '/image.png',
+  price: 'from $12.99',
+  brand: 'Adidas',
+  createdAt: '2020-02-11 00:00:00',
+  isActive: true,
+}
+const setUpProduct = (props = defaultProduct) => render(<ProductTile {...props as any} />)
+
 describe('The <ProductTile /> component', () => {
-  it('❌ renders a product tile with name, image and price', () => {
-
+  it(' renders a product tile with name, image and price', () => {
+    const {name,price} = defaultProduct
+    const {getByAltText,getByText} = setUpProduct()
+    expect(getByText(name)).toBeInTheDocument()
+    expect(getByText(price)).toBeInTheDocument()
+    expect(getByAltText(name)).toBeInTheDocument() 
   })
 
-  it('❌ renders a product tile with name and price only', () => {
-
+  it('renders a product tile with name and price only', () => {
+    const{name,price} = defaultProduct
+    const {queryByAltText,getByText } = setUpProduct({...defaultProduct,image:null})
+    expect(getByText(name)).toBeInTheDocument()
+    expect(getByText(price)).toBeInTheDocument()
+    expect(queryByAltText(name)).toBeNull()
+    // debug(queryByAltText(name))
   })
 
-  it('❌ has no accessibility violations', async () => {
-
+  it('has no accessibility violations', async () => {
+    const {container} = setUpProduct()
+    const result = await axe(container)
+    expect(result).toHaveNoViolations()
   })
 })
