e9650ad7bc413add2d4fef85cbe39c14301dfe02
solomonakp <solomonakp@gmail.com>
2021-01-28 14:45:12 +0100

    mote testing on context

--- src/__tests__/unit/components/case-4-ProductStream.test.tsx
+++ src/__tests__/unit/components/case-4-ProductStream.test.tsx
@@ -4,7 +4,36 @@ import { render } from '@testing-library/react'
 import ProductStream from '../../../components/ProductStream'
 
 describe('The <ProductStream /> component', () => {
-  it('❌ renders a list of Product tiles for each product passed to it', async () => {})
+  const products = [
+    {
+      id: 12,
+      name: 'Adidas 1',
+      image: './image.png',
+      price: 'from $12.99',
+      brand: 'Adidas',
+      createdAt: '2020-02-11 00:00:00',
+      isActive: true,
+    },
+    {
+      id: 13,
+      name: 'Nike 1',
+      image: './image-1.png',
+      price: 'from $42.99',
+      brand: 'Nike',
+      createdAt: '2020-02-11 00:00:00',
+      isActive: true,
+    },
+  ]
+  
+  it('renders a list of Product tiles for each product passed to it',  () => {
+    const { queryAllByTestId} = render( <ProductStream products={products as any}/>) 
+    expect(queryAllByTestId('ProductTile')).toHaveLength(products.length)
+  
+  })
 
-  it('❌ has no accessibility violations', async () => {})
+  it('has no accessibility violations', async () => {
+    const {container} = render( <ProductStream products={products as any}/>) 
+    const result = await axe(container)
+    expect(result).toHaveNoViolations()
+  })
 })
--- src/__tests__/unit/components/case-5-FiltersWrapper.test.tsx
+++ src/__tests__/unit/components/case-5-FiltersWrapper.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { render, fireEvent } from '@testing-library/react'
 
 import useFilters from '../../../hooks/useFilters'
@@ -6,10 +6,44 @@ import useFilters from '../../../hooks/useFilters'
 import { FiltersContext } from '../../../context/filters'
 import { FiltersWrapper } from '../../../components/FiltersWrapper'
 
+const withConsumer = ()=> render (<FiltersWrapper>
+                                    <FiltersContext.Consumer>
+                                      {
+                                        ({showingFilters,toggleShowingFilters}) => {
+                                          return (
+                                            <button onClick= {toggleShowingFilters} data-testid="button">
+                                              {showingFilters.toString()}
+                                            </button>
+                                          )
+                                        }
+                                      }
+                                    </FiltersContext.Consumer>
+
+                                  </FiltersWrapper>)
+
+
 describe('The <FiltersWrapper /> component', () => {
-  it('❌ should render all children passed to it', () => {})
+  const wrapper = 
+  it('should render all children passed to it', () => {
+    const {getByTestId} = withConsumer()
+    // debug(getByText('I am a child'))
+    expect(getByTestId('button')).toBeInTheDocument()
 
-  it('❌ should update the filters context with correct state values', () => {})
+  })
 
-  it('❌ should update the body style to prevent scrolling when filter is toggled', () => {})
+  it('should update the filters context with correct state values', () => {
+    const {getByTestId} = withConsumer()
+    const button = (getByTestId('button'))
+    expect(button.textContent).toBe('false')
+    
+  })
+
+  it('should update the body style to prevent scrolling when filter is toggled', () => {
+    const {debug,getByTestId,container} = withConsumer()
+    const button = (getByTestId('button'))
+    fireEvent.click(button)
+    expect(document.body.style.overflow).toBe('hidden')
+    fireEvent.click(button)
+    expect(document.body.style.overflow).toBe('scroll')
+  })
 })
