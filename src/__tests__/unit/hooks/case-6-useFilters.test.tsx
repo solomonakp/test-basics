afdd3f61bb51bf6e956d658c0a9bb469a71f581b
solomonakp <solomonakp@gmail.com>
2021-01-29 12:20:24 +0100

    testing hooks

--- src/__tests__/unit/hooks/case-6-useFilters.test.tsx
+++ src/__tests__/unit/hooks/case-6-useFilters.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render,fireEvent} from '@testing-library/react'
 
 import { useFilters } from '../../../hooks/useFilters'
 import { FiltersContext } from '../../../context/filters'
 
+const result = {} as any 
+const TestHook = (params) => {
+ result.current = useFilters()
+ const {toggleShowingFilters} = result.current
+  return null
+}
+const setUpHook = () => (render(<TestHook/>))
+
 describe('The useFilters hook', () => {
-  it('❌ returns the current value of the filters context', () => {})
+  it('❌ returns the current value of the filters context', () => {
+    const {getByText,debug} = setUpHook()
+    const {current} = result 
+    expect(current.showingFilters).toBe(false)
+  })
 })
