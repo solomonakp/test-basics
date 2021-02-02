55376a6c90276f3372f174a64b1fd12adf5f8c86
solomonakp <solomonakp@gmail.com>
2021-02-02 22:23:25 +0100

    filterBy examples

--- src/__tests__/integration/app.test.tsx
+++ src/__tests__/integration/app.test.tsx
@@ -1,27 +1,43 @@
 import React from 'react'
 import { Axios } from '../../helpers/axios'
-import { render, fireEvent, act, waitFor } from '@testing-library/react'
+import {
+  render,
+  fireEvent,
+  act,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import { Provider as StoreProvider } from 'react-redux'
 import { build, fake } from '@jackfranklin/test-data-bot'
 
 import App from '../../components/App'
-import store, { createStore } from '../../store'
+import { createStore } from '../../store'
 import { FiltersWrapper } from '../../components/FiltersWrapper'
+import { debug } from 'console'
 
-const MyApp = () => {
-  return (
-    <StoreProvider store={store}>
+const setUpApp = () =>
+  render(
+    <StoreProvider store={createStore()}>
       <FiltersWrapper>
         <App />
       </FiltersWrapper>
-    </StoreProvider>
+    </StoreProvider>,
   )
-}
-
 describe('The app ', () => {
-  test('❌ it fetches and renders all products on the page', async () => {})
+  test('it fetches and renders all products on the page', async () => {
+    const { findAllByTestId } = setUpApp()
+    const product = await findAllByTestId('ProductTile')
+    expect(product).not.toHaveLength(0)
+  })
 
-  test('❌ it can open and close the filters panel', async () => {})
+  test('it can open and close the filters panel', async () => {
+    const { container, queryByTestId } = setUpApp()
+    const btn = queryByTestId('FilterButton')
+    fireEvent.click(btn)
+    expect(queryByTestId('CloseCanvas')).toBeInTheDocument()
+    fireEvent.click(container)
+    expect(queryByTestId('CloseCanvas')).toBeNull()
+  })
 
   test('❌ it can search products as user types in the search field', async () => {})
 })
